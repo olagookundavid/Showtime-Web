fdcbcc3640d7a4512d8e4bde0dd4e5d64e589b15
David OH <erijesudo@gmail.com>
2026-03-27 16:14:39 +0100

    fix

--- frontend/src/pages/news/NewsDetail.tsx
+++ frontend/src/pages/news/NewsDetail.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { useParams, Link } from 'react-router-dom';
 import { getNewsBySlug, getNews } from '../../services/api';
