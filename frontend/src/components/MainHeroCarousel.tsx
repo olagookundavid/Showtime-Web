f58cc9a0216e41dfa6faeb3f30f8dd411acfff5e
David OH <erijesudo@gmail.com>
2026-05-02 12:44:25 +0100

    fix

--- frontend/src/components/MainHeroCarousel.tsx
+++ frontend/src/components/MainHeroCarousel.tsx
@@ -1,10 +1,8 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 
 interface Slide {
     id: number;
     image: string;
-    link?: string;
 }
 
 export const MainHeroCarousel = () => {
